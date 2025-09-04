49bf0fab680e3cedf126002848d40b645b29e8df
gouibrtd7fwr <haitran050414@gmail.com>
2025-09-04 18:57:03 +0700

    dev-6b

--- games/textAdventure_2/character.py
+++ games/textAdventure_2/character.py
@@ -1,16 +1,13 @@
 from utils import get_input
-import inquirer
 from data.items import *
 
-class Player:
+class Character:
     def __init__(self, name, start_pos):
         # Initialize player attributes:
         self.name = name
         self.health = self.max_hp = {"base": 100, "temp": 0}
         self.damage = {"base": 5, "temp": 0}
         self.weapon = "Fists"
-        self.inventory = []
-        self.cash = 0
         self.armor = [
             {"type": "head", "name": "", "strength": 0},
             {"type": "body", "name": "", "strength": 0},
@@ -18,7 +15,6 @@ class Player:
             {"type": "boot", "name": "", "strength": 0}
         ]
         self.position = start_pos
-        self.visited_rooms = [(0,0)]
         self.total_str = {"base": 0, "temp": 0}#used for further calculations.
         # - position (start_pos)
         # - gold = 100
@@ -36,47 +32,11 @@ class Player:
             self.total_str["base"] += piece["stats"]
         pass
 
-    def update_position(self, new_pos):
-        self.position = new_pos
-        self.visited_rooms.append(new_pos)
-
-
-    def show_inventory(self):
-        # Show inventory menu:
-        if len(self.inventory) == 0:
-            print('You have nothing in your inventory.')
-        else:
-            for i in range(len(self.inventory)):
-                print(f"{self.inventory[i]['name']}")
-            usage = get_input("Do you want to use an item? (y/n)")
-            if usage == "y":
-                choices = [(item['name'], item) for item in self.inventory]
-
-                questions = [
-                    inquirer.List(
-                        "item_chosen",
-                        message = "Which item do you want to choose?",
-                        choices = choices
-                    )
-                ]
-
-                answers = inquirer.prompt(questions)
-                selected = answers["choice"]
-                index = choices.index(selected)
-        get_input("Press space to continue.")
-        # - If empty: show message
-        # - Else: list all items with name and effect
-        # below is later implementation
-        # - Let user choose one to use
-        # - If item is usable, apply effect and remove from inventory
-        # - Otherwise, inform the player it's passive
-        pass
-
     def show_stats(self):
         self.calc_total_str()
-        print('-- STATS --')
+        print("-- Character's Stats --")
         # print('* means temporary effects!')
-        print(f"HP: {self.health["base"]}, Damage: {self.damage["base"]}, Defense: {self.total_str["base"]}, Coins: {self.cash}")
+        print(f"HP: {self.health["base"]}, Damage: {self.damage["base"]}, Defense: {self.total_str["base"]}")
         # Display player stats:
         # - HP, attack, defense, gold
         # - Indicate temporary effects with an asterisk
@@ -87,7 +47,3 @@ class Player:
     #     - Subtract temp_attack and temp_defense from current stats
     #     - Reset temp values to 0
     #     pass
-
-    def add_item(self, item):
-        self.inventory.append(item)
-        pass
